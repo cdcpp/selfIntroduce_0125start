2cf514b3b2f8112371ab2e4a04ac7b56b83c9a90
Ministar <Ministar@DESKTOP-KRKST06>
2021-01-25 21:47:45 +0900

    ff

---
+++ selfIntro/src/test/java/selfIntro/main/persistence/JDBCTests.java
@@ -0,0 +1,35 @@
+package selfIntro.main.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+	static {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try (Connection con = 
+				DriverManager.getConnection(
+						"jdbc:mariadb://HOST_ADDRESS:PORT_NUMBER/DB_NAME?user=USER_NAME&password=PASSWORD")) {
+			Log4j.info(con);
+			if(con != null) {
+				System.out.println("DB Connection Success!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+}
