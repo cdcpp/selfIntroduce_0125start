06430d370f46a5f6d1fc58fd7ddfd75c8c4daf00
Ministar <Ministar@DESKTOP-KRKST06>
2021-04-14 14:15:34 +0900

    send email

--- selfIntro/src/main/java/selfIntro/login/controller/LoginController.java
+++ selfIntro/src/main/java/selfIntro/login/controller/LoginController.java
@@ -10,12 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.log4j.Log4j;
+import selfIntro.login.domain.EmailVO;
 import selfIntro.login.domain.LoginVO;
 import selfIntro.login.service.LoginService;
 
@@ -26,34 +28,37 @@ public class LoginController {
 
 	@Inject
 	LoginService service;
-	
-	
+
 	// 미니보드 페이지 이동 Controller
 	@RequestMapping("/pageMiniBoard.do")
 	public ModelAndView moveMiniboard(ModelAndView mv) throws Exception {
-	
+
 		mv.setViewName("miniBoard/pageMiniBoard");
 		return mv;
-		
+
 	}
+
+	// 로그인 아이디 비밀번호 Check
+	@RequestMapping(value = "/doLogin.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String doLogin(@ModelAttribute LoginVO loginVO, HttpServletRequest request, ModelAndView mv,
+			HttpServletResponse response) throws Exception {
+
+		String loginCheck = service.doLogin(loginVO, request, mv, response);
+
+		return loginCheck;
+
+	}
+
+	// 회원가입 메일보내기
+	@RequestMapping(value = "/sendJoinEmail.do", method = RequestMethod.POST)
+	@ResponseBody
+	public int sendJoinEmail(@RequestBody EmailVO emailVO , HttpServletRequest request) throws Exception {
+
+		int sendEmail = service.sendJoinEmail(emailVO,request);
 		
-	//  로그인 아이디 비밀번호 Check 
-		@RequestMapping(value="/doLogin.do", method=RequestMethod.POST)
-		@ResponseBody
-		public String doLogin(@ModelAttribute LoginVO loginVO,HttpServletRequest request,ModelAndView mv,HttpServletResponse response) throws Exception {
-			
-		
-			
-			
-			String loginCheck = service.doLogin(loginVO, request, mv,response);
-			
-			
-			
-			return loginCheck;
-			
-		}
-	
-	
-	
-	
+		return sendEmail;
+
+	}
+
 }
--- selfIntro/src/main/java/selfIntro/login/service/impl/LoginServiceImpl.java
+++ selfIntro/src/main/java/selfIntro/login/service/impl/LoginServiceImpl.java
@@ -1,92 +1,121 @@
 package selfIntro.login.service.impl;
 
+import java.util.Properties;
+
 import javax.inject.Inject;
-import javax.servlet.http.Cookie;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.CookieGenerator;
 
+import selfIntro.login.domain.EmailVO;
 import selfIntro.login.domain.LoginVO;
 import selfIntro.login.mapper.LoginMapper;
 import selfIntro.login.service.LoginService;
-
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
 	@Inject
 	LoginMapper mapper;
-	
-	
 
 	@Override
-	public String doLogin(LoginVO loginVO, HttpServletRequest request,  ModelAndView mv,HttpServletResponse response) throws Exception {
-		
-		loginVO.setMemberId((String)request.getParameter("memberId"));
-		loginVO.setMemberPwd((String)request.getParameter("memberPwd"));
-		loginVO.setCheckSaveId((String)request.getParameter("checkSaveId"));
-		
-		
+	public String doLogin(LoginVO loginVO, HttpServletRequest request, ModelAndView mv, HttpServletResponse response)
+			throws Exception {
+
+		loginVO.setMemberId((String) request.getParameter("memberId"));
+		loginVO.setMemberPwd((String) request.getParameter("memberPwd"));
+		loginVO.setCheckSaveId((String) request.getParameter("checkSaveId"));
+
 		String memberId = "";
-		
+
 		try {
-			
+
 			memberId = mapper.loginCheck(loginVO);
-				
-			if(memberId == null || memberId == "") {
-				
+
+			if (memberId == null || memberId == "") {
+
 				memberId = "false";
-				
-				
-			}else {
-				
+
+			} else {
+
 				String checkSaveId = loginVO.getCheckSaveId();
-				
+
 				CookieGenerator cg = new CookieGenerator();
 
 				cg.setCookieName("user_id");
-				
-				
 
-				
-				
-				if(checkSaveId == null || checkSaveId == "") {
-					
+				if (checkSaveId == null || checkSaveId == "") {
+
 					cg.setCookieMaxAge(0);
-					
-				}else {
-					
-					
-					cg.setCookieMaxAge(30 * 24 * 60 * 60); //한달
-					
+
+				} else {
+
+					cg.setCookieMaxAge(30 * 24 * 60 * 60); // 한달
+
 				}
-				
-				cg.addCookie(response, loginVO.getMemberId());				
-	
-			}// else end
+
+				cg.addCookie(response, loginVO.getMemberId());
+
+			} // else end
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return memberId;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
-	
+
+	@Override
+	public int sendJoinEmail(EmailVO emailVO, HttpServletRequest request) throws Exception {
+		
+		System.out.println(emailVO);
+		
+		
+		String joinEmail = emailVO.getJoinEmail();
+		String joinPassword = emailVO.getJoinPassword();
+
+		final String user = "oblilic@naver.com"; // 발신자의 이메일 아이디를 입력
+		final String password = "ppap"; 		 // 발신자 이메일의 패스워드를 입력
+
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.naver.com");
+		prop.put("mail.smtp.port", 465);
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.ssl.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.naver.com");
+		
+		Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+		
+		try { 
+				MimeMessage message = new MimeMessage(session); 
+				message.setFrom(new InternetAddress(user));
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress("oblilic93@gmail.com")); // 메일 제목 
+				message.setSubject("이메일="+joinEmail+"패스워드="+joinPassword); // 메일 내용 
+				message.setText("PFP Success"); // send the message 
+				Transport.send(message);
+				System.out.println("Success Message Send"); 
+			} catch (MessagingException e) 
+			{ 
+				e.printStackTrace(); 
+			}
+		
+
+	return 0;
+}
+
 }
